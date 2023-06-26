5b5acbafc7323c837739185b0fae5d125cb21d6e
Yury Vishnevsky <yrvish03@bk.ru>
2023-06-26 12:47:00 +0300

    added transpose tests, modified create_matrix

--- src/matrix_tests.c
+++ src/matrix_tests.c
@@ -933,6 +933,105 @@ START_TEST(test_transpose_2)
 }
 END_TEST
 
+START_TEST(test_transpose_3)
+{
+  matrix_t A = {0}, result;
+  int rows = 3, columns = 5;
+  s21_create_matrix(rows, columns, &A);
+  for (int i = 0; i < rows; i++)
+  {
+    for (int j = 0; j < columns; j++)
+    {
+      A.matrix[i][j] = (double)(rand()) / RAND_MAX;
+    }
+  }
+  ck_assert_int_eq(s21_transpose(&A, &result), 0);
+  s21_remove_matrix(&A);
+  s21_remove_matrix(&result);
+}
+END_TEST
+
+START_TEST(test_transpose_4)
+{
+  matrix_t A = {0}, result;
+  int rows = 31, columns = 52;
+  s21_create_matrix(rows, columns, &A);
+  for (int i = 0; i < rows; i++)
+  {
+    for (int j = 0; j < columns; j++)
+    {
+      A.matrix[i][j] = (double)(rand()) / RAND_MAX;
+    }
+  }
+  ck_assert_int_eq(s21_transpose(&A, &result), 0);
+  s21_remove_matrix(&A);
+  s21_remove_matrix(&result);
+}
+END_TEST
+
+START_TEST(test_transpose_5)
+{
+  matrix_t A = {0}, result = {0};
+  int rows = 51, columns = 5;
+  s21_create_matrix(rows, columns, &A);
+  for (int i = 0; i < rows; i++)
+  {
+    for (int j = 0; j < columns; j++)
+    {
+      A.matrix[i][j] = (double)(rand()) / RAND_MAX;
+    }
+  }
+  ck_assert_int_eq(s21_transpose(&A, &result), 0);
+  s21_remove_matrix(&A);
+  s21_remove_matrix(&result);
+}
+END_TEST
+
+START_TEST(test_transpose_6)
+{
+  matrix_t A = {0}, result = {0};
+  A.matrix = NULL;
+  ck_assert_int_eq(s21_transpose(&A, &result), 1);
+}
+END_TEST
+
+START_TEST(test_transpose_7)
+{
+  matrix_t A = {0}, result = {0}, result_tmp = {0};
+  int rows = 3, columns = 2;
+  int sum = 0;
+  s21_create_matrix(rows, columns, &A);
+  A.matrix[0][0] = 1;
+  A.matrix[0][1] = 4;
+  A.matrix[1][0] = 2;
+  A.matrix[1][1] = 5;
+  A.matrix[2][0] = 3;
+  A.matrix[2][1] = 6;
+  s21_create_matrix(columns, rows, &result_tmp);
+  result_tmp.matrix[0][0] = 1;
+  result_tmp.matrix[0][1] = 2;
+  result_tmp.matrix[0][2] = 3;
+  result_tmp.matrix[1][0] = 4;
+  result_tmp.matrix[1][1] = 5;
+  result_tmp.matrix[1][2] = 6;
+  s21_transpose(&A, &result);
+  for (int i = 0; i < columns; i++)
+  {
+    for (int j = 0; j < rows; j++)
+    {
+      if (fabs(result_tmp.matrix[i][j] - result.matrix[i][j]) > 1e-8)
+      {
+        sum++;
+      }
+    }
+  }
+  ck_assert_int_eq(sum, 0);
+  s21_remove_matrix(&A);
+  s21_remove_matrix(&result);
+  s21_remove_matrix(&result_tmp);
+}
+END_TEST
+
 // calc complements
 
 START_TEST(test_calc_complements_1)
@@ -1311,12 +1410,19 @@ Suite *suite_transpose()
   Suite *s = suite_create("suite_transpose");
   TCase *tc_1 = tcase_create("tc_1");
   TCase *tc_2 = tcase_create("tc_2");
+  TCase *tc_3 = tcase_create("tc_3");
 
   tcase_add_test(tc_1, test_transpose_1);
   tcase_add_test(tc_2, test_transpose_2);
+  tcase_add_test(tc_3, test_transpose_3);
+  // tcase_add_test(tc_3, test_transpose_4);
+  // tcase_add_test(tc_3, test_transpose_5);
+  tcase_add_test(tc_3, test_transpose_6);
+  // tcase_add_test(tc_3, test_transpose_7);
 
   suite_add_tcase(s, tc_1);
   suite_add_tcase(s, tc_2);
+  suite_add_tcase(s, tc_3);
 
   return s;
 }
--- src/s21_create_matrix.c
+++ src/s21_create_matrix.c
@@ -5,10 +5,10 @@ int s21_create_matrix(int rows, int columns, matrix_t *result)
     return INCORRECT_MATRIX;
   result->rows = rows;
   result->columns = columns;
-  result->matrix = malloc(columns * sizeof(double *));
-  for (int i = 0; i < columns; i++)
+  result->matrix = malloc(rows * sizeof(double *));
+  for (int i = 0; i < rows; i++)
   {
-    result->matrix[i] = malloc(rows*sizeof(double)); // инициализация calloc
+    result->matrix[i] = malloc(columns * sizeof(double)); // инициализация calloc
   }
   return OK;
 }
